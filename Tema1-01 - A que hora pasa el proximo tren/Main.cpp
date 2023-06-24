3d6abc3353d5169976036ede2a9651112fc4cdbb
Daniel-Fdez17 <94752973+Daniel-Fdez17@users.noreply.github.com>
2023-06-24 18:38:46 +0200

    CORRECT

--- Tema1-01 - A que hora pasa el proximo tren/Main.cpp
+++ Tema1-01 - A que hora pasa el proximo tren/Main.cpp
@@ -1,41 +1,40 @@
-﻿#include <iostream>
+#include <iostream>
 #include <iomanip>
 #include <fstream>
 #include "Hora.h"
 #include<vector>
 using namespace std;
-// El coste del algoritmo es logaritmico en v.size() (busqueda binaria)
-void resolver(const vector<Hora>& v, Hora const& h, Hora& out, int c, int f) {
-    // CB: Un elemento
-    if (c == f) {
-        if (h < v[c] || h == v[c]) out = v[c];
-    }
-    // CB: Dos elementos
-    else if (c == f - 1) {
-        if (h < v[c] || h == v[c]) out = v[c];
-        else if (h < v[f] || h == v[f]) out = v[f];
-    }
-    // CR
-    else {
-        int m = (c + f) / 2;
-        (v[m] < h ? resolver(v, h, out, m + 1, f)
-            : resolver(v, h, out, 0, m));
-    }
-}
-// El coste del algorimto es lineal en v.size() == trenes
-pair<bool, Hora> resolver(const vector<Hora>& v, const Hora& hora, int trenes) {
-    int i = 0; bool exit = false;
-    pair<bool, Hora> ret = { false, Hora() };
-    while (i < trenes && !exit) {
-        if (hora < v[i] || hora == v[i]) {
-            ret.first = true;
-            ret.second = v[i];
-            exit = true;
+// Al utilizar el algoritmo de busqueda binaria, el coste del mismo pertenece a O(log(v.size())) en el caso peor (esta en un extremo)
+// En el caso mejor es constante porque entra directamente en los CB o es el elemento del medio
+void buscar_hora(vector<Hora>const&v, Hora const&h, Hora&sol, bool&encontrado, int c, int f) {
+    if (encontrado) return;
+    if (c == f) { // CB: un elemento
+        if (h < v[c] || h == v[c]) {
+            encontrado = true;
+            sol = v[c];
         }
-        i++;
     }
-    return ret;
+    else if (c == f - 1) { // CB: dos elementos
+        if (h < v[c] || h == v[c]) {
+            encontrado = true;
+            sol = v[c];
+        }
+        else if (h < v[f] || h == v[f]) {
+            encontrado = true;
+            sol = v[f];
+        }
+    }
+    else { // CR
+        int m = (c + f) / 2;
+        if (v[m] == h) {
+            encontrado = true;
+            sol = h;
+        }
+        else if (h < v[m]) buscar_hora(v,h,sol,encontrado,c,m);
+        else buscar_hora(v,h,sol,encontrado,m+1,f);
+    }
 }
+
 
 // Resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
@@ -43,19 +42,15 @@ bool resuelveCaso() {
     int trenes, horas; cin >> trenes >> horas;
     if (trenes == 0 && horas == 0) return false;
     vector<Hora>v(trenes);
-    for (int i = 0; i < trenes; i++)
-        cin >> v[i];
-    for (int i = 0; i < horas; i++) {
+    for (Hora&h : v) cin >> h;
+        for (int i = 0; i < horas; i++) {
         try {
-            Hora hora, sol, aux; cin >> hora;
-            resolver(v, hora, sol, 0, trenes - 1);
-            if (!(sol == aux)) cout << sol << "\n";
+            Hora h, sol; cin >> h;
+            bool encontrado = false;
+            buscar_hora(v,h,sol,encontrado,0,trenes-1);
+            if (encontrado) cout << sol << "\n";
             else cout << "NO\n";
-            //pair<bool, Hora> sol = resolver(v, hora, trenes);
-            //if (sol.first) cout << sol.second << "\n";
-            //else cout << "NO\n";
-        }
-        catch (Error e) { cout << e.getMsg() << "\n"; }
+        } catch (Error e) { cout << e.getMsg() << "\n"; }
     }
     cout << "---\n";
     return true;
